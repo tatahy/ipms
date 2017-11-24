448df7575d5b38865695db7566cab8263697cf3c
tatahy <hy135@139.com>
2017-11-24 10:54:31 +0800

    patent模块前台显示页面重构。fix #3

--- application/patent/controller/IndexController.php
+++ application/patent/controller/IndexController.php
@@ -9,6 +9,7 @@ use app\patent\model\Patinfo as PatinfoModel;
 use app\patent\model\Patrecord as PatrecordModel;
 use app\issue\model\Issinfo as IssinfoModel;
 use app\attachment\model\Attinfo as AttinfoModel;
+use app\admin\model\Dept as DeptModel;
 
 class IndexController extends \think\Controller
 {
@@ -1027,5 +1028,287 @@ class IndexController extends \think\Controller
          return view();
 
     }
+    
+    // 输出total模板
+    public function total(Request $request)
+    {
+      
+      //通过$log判断是否是登录用户，非登录用户退回到登录页面
+      if(1!==$this->log){
+        $this->error('未登录用户，请先登录系统');
+      //$this->redirect($request->domain());
+      }else{
+        //$totalTableRows接收前端页面传来的分页时每页显示的记录数，默认为10
+        if(!empty($request->param('totalTableRows'))){
+          $totalTableRows=$request->param('totalTableRows');
+        }else{
+          $totalTableRows=10;
+        }
+        
+         // 接收前端分页页数变量：“pageUserNum”
+        if(!empty($request->param('pageTotalNum'))){
+          $pageTotalNum=$request->param('pageTotalNum');
+        }else{
+          $pageTotalNum=1;
+        }
+        
+        // $sortName接收前端页面传来的排序字段名
+        if(!empty($request->param('sortName'))){
+          $sortName=$request->param('sortName');
+        }else{
+          $sortName='_PATNAME';
+        }
+        
+        // $sort接收前端页面传来的排序顺序
+        if(!empty($request->param('sort'))){
+          $sort=$request->param('sort');
+        }else{
+          $sort='_ASC';
+        }
+        
+        // $patStatus接收前端页面传来的专利状态值
+        if(!empty($request->param('patStatus'))){
+          $patStatus=$request->param('patStatus');
+        }else{
+          $patStatus=0;
+        }
+        
+         // 查询词1，'searchPatName'
+        if(!empty($request->param('searchPatName'))){
+          $searchPatName=$request->param('searchPatName');
+        }else{
+          $searchPatName='';
+        } 
+        
+        // 查询词2，'searchDept'
+        if(!empty($request->param('searchDept'))){
+          $searchDept=$request->param('searchDept');
+        }else{
+          $searchDept=0;
+        } 
+        
+        // 查询词3，'searchPatStatus'
+        if(!empty($request->param('searchPatStatus'))){
+          $searchPatStatus=$request->param('searchPatStatus');
+        }else{
+          $searchPatStatus=0;
+        }
+        
+        // 查询词4，'searchPatType'
+        if(!empty($request->param('searchPatType'))){
+          $searchPatType=$request->param('searchPatType');
+        }else{
+          $searchPatType=0;
+        } 
+        
+        // 查询词5，'searchWriter'
+        if(!empty($request->param('searchWriter'))){
+          $searchWriter=$request->param('searchWriter');
+        }else{
+          $searchWriter='';
+        }  
+        
+      }
+      
+      // 选择排序字段
+      switch($sortName){
+        case '_PATNAME':
+          $strOrder='topic';
+        break;
+            
+        case '_PATTYPE':
+          $strOrder='pattype';
+        break;
+        
+        case '_AUTHOR':
+          $strOrder='author';
+        break;
+        
+        case '_INVENTOR':
+          $strOrder='inventor';
+        break;
+            
+        case '_PATOWNER':
+          $strOrder='patowner';
+        break;
+            
+        case '_SUBMITDATE':
+          $strOrder='submitdate';
+        break;
+            
+        case '_DEPT':
+          $strOrder='dept';
+        break;
+            
+        case '_PROJECT':
+          $strOrder='pronum';
+        break;
+        
+        case '_PATSTATUS':
+          $strOrder='status';
+        break;
+            
+        //默认按字段“topic”
+        default:
+          $strOrder='topic';  
+          $sortName="_PATNAME";
+        break;
+      } 
+      
+      //  组合升序or降序查询
+      if($sort=="_ASC"){
+          $strOrder=$strOrder.' asc';
+      }else{
+          $strOrder=$strOrder.' desc';
+          
+      }
+      
+      // 组合状态查询条件，
+      switch($patStatus){
+        case '#total':
+          $map='';
+          
+          // 5个查询词
+          if($searchDept){
+            $map['dept']=$searchDept;
+          }
+          
+          if($searchPatName){
+            $map['topic']=['like','%'.$searchPatName.'%'];
+          }
+          
+          if($searchPatStatus){
+            $map['status']=$searchPatStatus;
+          }
+          
+          if($searchPatType){
+            $map['pattype']=$searchPatType;
+          }
+          
+          if($searchWriter){
+            $map['author']=['like','%'.$searchWriter.'%'];
+          }
+          
+        break;
+        // '新增'   
+        case '#newAdd':
+          $map['status'] ='新增';
+        break;
+        // ''    
+        case '#apply':
+          $map['status'] =['in',['返回修改','申报']];
+        break;
+        // ''    
+        case '#authorize':
+          $map['status'] ='授权';
+        break;
+        // ''    
+        case '#render':
+          $map['status'] =['in',['续费授权','续费']];
+        break;
+        // ''    
+        case '#abandon':
+          $map['status'] ='放弃';
+        break;
+        // ''
+        case '#reject':
+          $map['status'] ='驳回';
+        break;
+            
+        //默认所有状态专利'#total':
+        default:
+          $map='';
+        break;
+      }  
+         
+      //使用模型Patinfo
+      $pats = new PatinfoModel;
+      
+      // 查出所有的用户并分页，根据“strOrder”排序，前端页面显示的锚点（hash值）为$fragment，设定分页页数变量：“pageTotalNum”
+      // 带上每页显示记录行数$totalTableRows，实现查询结果分页显示。
+      $patTotal = $pats->where('id','>',0)
+                        ->where($map)
+                        ->order($strOrder)
+                        ->paginate($totalTableRows,false,['type'=>'bootstrap','fragment'=>$patStatus,'var_page' => 'pageTotalNum',
+                        'query'=>['totalTableRows'=>$totalTableRows]]);
+                        
+      // 获取分页显示
+      $pageTotal = $patTotal->render();
+      // 记录总数
+      $numTotal = $pats->where('id','>',0)->where($map)->count();
+      
+      $this->assign([
+              'home'=>$request->domain(),
+              
+              // 分页显示所需参数
+              'patTotal'=>$patTotal,
+              'numTotal'=>$numTotal,
+              'pageTotal'=>$pageTotal,
+              'totalTableRows'=>$totalTableRows,
+              'pageTotalNum'=>$pageTotalNum,
+              
+              // 表格搜索字段
+              'searchPatName'=>$searchPatName,
+              'searchDept'=>$searchDept,
+              'searchPatStatus'=>$searchPatStatus,
+              'searchPatType'=>$searchPatType,
+              'searchWriter'=>$searchWriter,
+              
+    
+              
+              // 表格排序信息
+              'sortName'=>$sortName,
+              'sort'=>$sort,
+              'totalTableRows'=>$totalTableRows,
+              
+              // 所return的页面显示的pat状态值$patStatus
+              'patStatus'=>$patStatus,
+              
+        ]);
+      return view();
+    }
+    
+    // 获取所有部门信息,不能写成“_dept”，因为前端的HTML文件中的url里不能含有“_”开头的名称，否则就无法访问到，会报错
+    public function dept()
+    {
+      //通过$log判断是否是登录用户，非登录用户退回到登录页面
+        if(1!==$this->log){
+            $this->error('未登录用户，请先登录系统');
+            //$this->redirect($request->domain());
+        }else{
+            $dept=DeptModel::all();
+            // 将数组转化为json
+            return json($dept);
+        }
+    }
+    
+    // 获取所有专利状态信息,不能写成“_status”，因为前端的HTML文件中的url里不能含有“_”开头的名称，否则就无法访问到，会报错
+    public function patStatus()
+    {
+      //通过$log判断是否是登录用户，非登录用户退回到登录页面
+        if(1!==$this->log){
+            $this->error('未登录用户，请先登录系统');
+            //$this->redirect($request->domain());
+        }else{
+            $status=PatinfoModel::field('id,status')->group('status')->select();
+            // 将数组转化为json
+            return json($status);
+        }
+    }
+    
+    // 获取所有专利类型信息,不能写成“_type”，因为前端的HTML文件中的url里不能含有“_”开头的名称，否则就无法访问到，会报错
+    public function patType()
+    {
+      //通过$log判断是否是登录用户，非登录用户退回到登录页面
+        if(1!==$this->log){
+            $this->error('未登录用户，请先登录系统');
+            //$this->redirect($request->domain());
+        }else{
+            $type=PatinfoModel::field('id,pattype')->group('pattype')->select();
+            // 将数组转化为json
+            return json($type);
+        }
+    }
+    
         
 }
