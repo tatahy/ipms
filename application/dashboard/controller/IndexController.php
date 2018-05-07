b2722a9fcceaef4a93663e7e6931aa16bf652607
tatahy <hy135@139.com>
2018-05-07 17:16:20 +0800

    application/dashboard update.

--- application/dashboard/controller/IndexController.php
+++ application/dashboard/controller/IndexController.php
@@ -60,7 +60,7 @@ class IndexController extends \think\Controller
       }    
     }
     
-    public function index(Request $request)
+    public function index(Request $request,$auth='done')
     {
        $this->_loginUser();
        
@@ -74,8 +74,6 @@ class IndexController extends \think\Controller
         
         if(!empty($request->param('auth'))){
            $auth=$request->param('auth');
-        }else{
-            $auth='_DONE';
         }
         
          //传来的role值是否在$roles中
@@ -116,34 +114,34 @@ class IndexController extends \think\Controller
         
         if($this->auth['iss']['edit']){
           $numIssPatEdit=$issSet->where($mapEdit)->count(); 
-          $auth='_EDIT';
+          $auth='edit';
         }else{
           $numIssPatEdit=0;
         }
         
         if($this->auth['iss']['audit']){
           $numIssPatAudit=$issSet->where($mapAudit)->count(); 
-          $auth='_AUDIT';
+          $auth='audit';
         }else{
           $numIssPatAudit=0;
         }
         
         if($this->auth['iss']['approve']){
           $numIssPatApprove=$issSet->where($mapApprove)->count(); 
-          $auth='_APPROVE';
+          $auth='approve';
         }else{
           $numIssPatApprove=0;
         }
         
         if($this->auth['iss']['execute']){
           $numIssPatExecute=$issSet->where($mapExecute)->count();
-          $auth='_EXECUTE'; 
+          $auth='execute'; 
         }else{
           $numIssPatExecute=0;
         }
         
         if($this->auth['iss']['maintain']){
-          $auth='_MAINTAIN';
+          $auth='maintain';
           $numIssPatMaintain=$issSet->where($mapMaintain)->count(); 
           //得到满足续费条件的专利数
           $today=date('Y-m-d');
@@ -380,27 +378,25 @@ class IndexController extends \think\Controller
         $tplFile='#issPat';
       }
       
+      if(!empty($request->param('auth'))){
+        $auth=$request->param('auth');
+      }else{
+        $auth='';
+      }
+      
       //返回模板文件
       if(substr($tplFile,0,1)=='#'){
         $tplFile=substr($tplFile,1);
-        $this->redirect($tplFile);
+        $this->redirect($tplFile,['auth' =>$auth]);
       }else{
         return '模板文件不存在。';
       }
     
     }
     
-     public function issPat(Request $request)
+     public function issPat(Request $request,$auth='done')
     {
        $this->_loginUser();
-      
-      //从session中取出登录用户的关键信息
-        $username=Session::get('username');
-        $pwd=Session::get('pwd');
-        $log=Session::get('log');
-        $roles=Session::get('role');
-        $dept=Session::get('dept');
-        $role=$request->param('role');
       
       //使用模型Issinfo
         $issSet = new IssinfoModel; 
@@ -424,11 +420,17 @@ class IndexController extends \think\Controller
         //done
         $map['status'] ='完结';
         
-        // $auth接收前端页面传来的auth值,表示rolename（映射“用户组名”）
+        // $auth接收前端页面传来的auth值
         if(!empty($request->param('auth'))){
           $auth=$request->param('auth');
         }else{
-          $auth='_DONE';
+          foreach($this->auth['iss'] as $key=>$value){
+            if($value){
+              $auth=$key;
+              break;
+            }
+          }
+          
         }
         
         if($this->auth['iss']['edit']){
@@ -477,10 +479,9 @@ class IndexController extends \think\Controller
         $numTotal=$numIssPatEdit+$numIssPatAudit+$numIssPatApprove+$numIssPatExecute+$numIssPatMaintain+$numPatRenewTotal;
         
         $destr= "请求方法:".$request->method()."<br/>".
-                "username:". $username."<br/>".
-                "pwd:".$pwd."<br/>".
-                "roles:".$roles[0]."<br/>".
-                "log:".$log."<br/>".
+                "username:". $this->username."<br/>".
+                "pwd:".$this->pwd."<br/>".
+                "log:".$this->log."<br/>".
                 "auth:".json_encode($this->auth); 
         
          // 模板变量赋值        
@@ -488,15 +489,9 @@ class IndexController extends \think\Controller
           //在usercenter.html页面输出自定义的信息
           //在index.html页面通过destr输出自定义的信息
           'destr'=>$destr."</br>",
-          //在index.html页面通过array输出自定义的数组内容
-          'array'=>$roles, 
           
           'home'=>$request->domain(),
-          'username'=>$username,
-          'roles'=>$roles,
-          //'roles'=>json($roles),
-          'role'=>$role,
-          'role1st'=>$roles[0],
+          'username'=>$this->username,
   
           //向前端权限变量赋值
           //所有权限
@@ -520,15 +515,13 @@ class IndexController extends \think\Controller
     } 
     
     //根据前端传来的权限，选择返回前端的模板文件及内容
-    public function issPatAuth(Request $request)
+    public function issPatAuth(Request $request,$auth='done')
     {
       $this->_loginUser();
       
       // $role接收前端页面传来的auth值
       if(!empty($request->param('auth'))){
         $auth=$request->param('auth');
-      }else{
-        $auth='_DONE';
       }
       
       // $returnType接收前端页面传来的returnType值，‘0’为模板文件，‘1’为数据
@@ -658,29 +651,29 @@ class IndexController extends \think\Controller
       }
       //选择模板文件名,组合查询条件
       switch($auth){
-          //_EDIT
-          case '_EDIT':
+          //edit
+          case 'edit':
             $map['status'] =['in',['填报','返回修改','修改完善']];
             $map['dept'] =$this->dept;
             $map['writer']=$this->username;
             //$tplFile='issPatEdit';
             $tplFile='edit';
           break;
-          //_AUDIT
-          case '_AUDIT':
+          //audit
+          case 'audit':
             $map['status'] ='待审核';
             $map['dept'] =$this->dept;
             //$tplFile='issPatAudit';
             $tplFile='audit';
           break;
-          //_APPROVE
-          case '_APPROVE':
+          //approve
+          case 'approve':
             $map['status'] =['in',['审核未通过','审核通过','变更申请','拟续费']];
             //$tplFile='issPatApprove';
             $tplFile='approve';
           break;
-          //_EXECUTE
-          case '_EXECUTE':
+          //execute
+          case 'execute':
             $map['executer'] =$this->username;
             $map['status'] =['in',['批准申报','申报执行','申报修改','准予变更','否决变更']];
             $map['dept'] =$this->dept;
@@ -688,8 +681,8 @@ class IndexController extends \think\Controller
             //$tplFile='issPatExecute';
             $tplFile='execute';
           break;
-          //_MAINTAIN
-          case '_MAINTAIN':
+          //maintain
+          case 'maintain':
             $map['status'] =['in',['申报复核','申报提交','续费提交','准予续费',
                                       '否决申报','专利授权','专利驳回','放弃续费','续费授权']];
             //$tplFile='issPatMaintain';
@@ -701,7 +694,7 @@ class IndexController extends \think\Controller
 //            //$tplFile='issPatMaintain';
 //            $tplFile='patRenew';
 //          break;
-          //_DONE
+          //done
           default:
             $map['status'] ='完结';
             //$tplFile='issPatDone';
@@ -761,7 +754,7 @@ class IndexController extends \think\Controller
     }
     
     //根据前端传来的权限，选择返回前端的模板文件及内容
-    public function issPatAuthSingle(Request $request)
+    public function issPatAuthSingle(Request $request,$auth='done')
     {
       $this->_loginUser();
       
@@ -774,33 +767,31 @@ class IndexController extends \think\Controller
       
       if(!empty($request->param('auth'))){
         $auth=$request->param('auth');
-      }else{
-        $auth='';
       }
       
       //选择模板文件名
       switch($auth){
-        //_EDIT
-        case '_EDIT':
+        //edit
+        case 'edit':
           $tplFile='editSingle';
         break;
-        //_AUDIT
-        case '_AUDIT':
+        //audit
+        case 'audit':
           $tplFile='auditSingle';
         break;
-        //_APPROVE
-        case '_APPROVE':
+        //approve
+        case 'approve':
           $tplFile='approveSingle';
         break;
-        //_EXECUTE
-        case '_EXECUTE':
+        //execute
+        case 'execute':
           $tplFile='executeSingle';
         break;
-        //_MAINTAIN
-        case '_MAINTAIN':
+        //maintain
+        case 'maintain':
           $tplFile='maintainSingle';
         break;
-        //_DONE
+        //done
         default:
           $tplFile='doneSingle';
         break;
@@ -810,7 +801,7 @@ class IndexController extends \think\Controller
       if($oprt=='_ADDNEW'){
         $iss=array('id'=>'','topic'=>'','abstract'=>'');
         //查询当前用户已上传的所有附件信息
-        $att= AttinfoModel::all(['attmap_id'=>0,'uploader'=>$this->username,'rolename'=>'_EDIT','deldisplay'=>1]);
+        $att= AttinfoModel::all(['attmap_id'=>0,'uploader'=>$this->username,'rolename'=>'edit','deldisplay'=>1]);
         $pat=array('id'=>'','topic'=>'','patowner'=>'','otherinventor'=>'','inventor'=>'');
         $patType=0;
       }else{
@@ -860,7 +851,7 @@ class IndexController extends \think\Controller
       if(!empty($request->param('auth'))){
         $auth=$request->param('auth');
       }else{
-        $auth='_DONE';
+        $auth='done';
       }
       
       // $patId接收前端页面传来的patId值
@@ -2066,7 +2057,7 @@ class IndexController extends \think\Controller
           'pat'=>$pat,
           'patRenewTotal'=>count($pat),
           'pageTotal'=>$pageTotal,
-          'auth'=>'_MAINTAIN'
+          'auth'=>'maintain'
       ]);
        
        //return '<div style="padding: 24px 48px;"><h1>:)</h1><p>_RENEW 模块开发中……<br/></p></div>';
