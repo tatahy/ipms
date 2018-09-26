ba4c70aeaa603e9e200f5340a14bd48ece3c25ee
tatahy <hy135@139.com>
2018-09-26 18:55:26 +0800

    update module: asset.

--- application/asset/common.php
+++ application/asset/common.php
@@ -1,14 +1,14 @@
 <?php
 
 //asset的状态数组
-const assStatusArr=['*','新增(待分配)',
-                '正常(折旧中)','正常(折旧完)',
-                '异常(待更新)','异常(待维修)','异常(遗失)',
-                '停用(维修中)','停用(封存)','停用(待销账)',
-                '销账'];
-                
-    function assStatusArr()
-    {
-        return assStatusArr;
-    }
+//const assStatusArr=['*','新增(待分配)',
+//                '正常(折旧中)','正常(折旧完)',
+//                '异常(待更新)','异常(待维修)','异常(遗失)',
+//                '停用(维修中)','停用(封存)','停用(待销账)',
+//                '销账'];
+//                
+//    function assStatusArr()
+//    {
+//        return assStatusArr;
+//    }
 
