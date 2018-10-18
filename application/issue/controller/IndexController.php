00585d2b8f6fc1e704b5df62788488c3a2337aa2
tatahy <hy135@139.com>
2018-10-18 17:38:56 +0800

    update.

--- application/issue/controller/IndexController.php
+++ application/issue/controller/IndexController.php
@@ -1035,6 +1035,7 @@ class IndexController extends \think\Controller
           'topic'=>$topic,
          
           'issrecords'=>$issrecords,
+          'totalNum'=>count($issrecords),
           'year'=>date('Y')
         ]);
          
